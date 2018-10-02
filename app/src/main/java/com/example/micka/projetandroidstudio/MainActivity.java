4bfdfc5b00132394c6f12290f8d197f25afc90c4
Mickaël <jobert@et.esiea.fr>
2018-10-02 14:49:12 +0200

    On passe au MVVMMVVMVMVMVM

--- app/src/main/java/com/example/micka/projetandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/micka/projetandroidstudio/MainActivity.java
@@ -1,55 +1,25 @@
 package com.example.micka.projetandroidstudio;
 
-
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
+import com.example.micka.projetandroidstudio.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button button1;
-    private Button button2;
-
+public class MainActivity extends AppCompatActivity implements Navigator{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-
-        button1 = (Button)findViewById(R.id.Button1);
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
-
-        button2 = (Button)findViewById(R.id.Button2);
-        button2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openActivity3();
-            }
-        });
-    }
-
-    public void openActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-    }
-
-    public void openActivity3(){
-        Intent intent = new Intent(this, MainActivity3.class);
-        startActivity(intent);
+        ActivityMainBinding b =((ActivityMainBinding) DataBindingUtil.setContentView(this, R.layout.activity_main));
+        b.setViewModel(new MainViewModel(this));
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,46 +28,28 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem Item){
-        switch(Item.getItemId()){
-            case R.id.exit:
-                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Exit ?");
-                builder.setCancelable(true);
-                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-                builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
-                break;
-
-            case R.id.settings:
-                Intent intent1 = new Intent(this, MainActivity4.class);
-                startActivity(intent1);
-                break;
-        }
-
-        return super.onOptionsItemSelected(Item);
+    public void goToActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        this.startActivity(intent);
     }
 
-    public void onBackPressed(){
-        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    @Override
+    public void goToActivity3() {
+        Intent intent = new Intent(this, MainActivity3.class);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Are you sure");
         builder.setCancelable(true);
         builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                finish();
+                MainActivity.this.finish();
             }
         });
         builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
@@ -109,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
-
-
 }
 
 
